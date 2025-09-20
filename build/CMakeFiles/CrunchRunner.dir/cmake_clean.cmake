c7b6e3df5b3a0e35667f5b8c3d8420febb3ce651
Murtaza Haque <murtaza0713@gmail.com>
2025-09-20 13:07:40 -0400

    CMake Config and Starter Files

---
+++ build/CMakeFiles/CrunchRunner.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CrunchRunner.dir/src/main.cpp.o"
+  "CMakeFiles/CrunchRunner.dir/src/main.cpp.o.d"
+  "CrunchRunner"
+  "CrunchRunner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CrunchRunner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CrunchRunner.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/CrunchRunner.dir/src/main.cpp.o: \
+ /home/darklynx/programming-projects/Crunch/Crunch/src/main.cpp \
+ /usr/include/stdc-predef.h
